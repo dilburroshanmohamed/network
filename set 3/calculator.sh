64914ced0d035996dfd2393f3a50768edd3da289
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2025-03-13 11:28:03 +0530

    Create calculator.sh

---
+++ set 3/calculator.sh
@@ -0,0 +1,11 @@
+echo "enter numbers"
+read a
+echo "enter number"
+read b
+echo "enter number"
+read c
+echo "enter number"
+read d
+result=$(expr $a \* 20 - $b \* 2 + $c / $d)
+echo "result is $result"
+~                                    
